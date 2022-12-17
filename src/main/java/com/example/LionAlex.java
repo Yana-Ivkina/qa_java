5b505af9e1f66c0c9d6912e3f3e409a1b0cdce5c
Yana-Ivkina <ja.ivkina@yandex.ru>
2022-12-17 18:56:18 +0500

    Создала класс LionAlex

---
+++ src/main/java/com/example/LionAlex.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.List;
+
+public class LionAlex extends Lion {
+
+    public LionAlex() throws Exception {
+        super("Самец");
+    }
+
+    @Override
+    public int getKittens() {
+        return 0;
+    }
+
+    public List<String> getFriends() {
+        return List.of("Марти", "Глория", "Мелман");
+    }
+
+    public String getPlaceOfLiving() {
+        return "Нью-Йоркский зоопарк";
+    }
+}
